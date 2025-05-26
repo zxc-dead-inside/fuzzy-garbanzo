e76b49fb0c6088ccbf7936601de618100086e232
zxc-dead-inside <maks.arhipkin@mail.ru>
2025-05-26 23:57:39 +1000

    init promo

--- config/settings/base.py
+++ config/settings/base.py
@@ -90,7 +90,8 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "market.users",
     "market.catalog",
-    "market.orders"
+    "market.orders",
+    "market.promo",
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
