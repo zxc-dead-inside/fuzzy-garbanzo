aa035bcc04174fede194e012217258a4eb6754e2
zxc-dead-inside <maks.arhipkin@mail.ru>
2025-05-27 00:11:05 +1000

    admin panel promos

--- market/promo/admin.py
+++ market/promo/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from .models import PercentPromo, NForMPromo, FixedPricePromo
 
-# Register your models here.
+
+@admin.register(PercentPromo)
+class PercentPromoAdmin(admin.ModelAdmin):
+    list_display = ['name', 'discount_percent', 'is_active',
+                    'start_date', 'end_date']
+    list_filter = ['is_active', 'start_date', 'end_date']
+    search_fields = ['name', 'description']
+    filter_horizontal = ['products']
+    fields = ['name', 'description', 'discount_percent', 'products',
+              'start_date', 'end_date', 'is_active']
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(promo_type='percent')
+
+
+@admin.register(NForMPromo)
+class NForMPromoAdmin(admin.ModelAdmin):
+    list_display = ['name', 'n', 'm', 'is_active', 'start_date', 'end_date']
+    list_filter = ['is_active', 'start_date', 'end_date']
+    search_fields = ['name', 'description']
+    filter_horizontal = ['products']
+    fields = ['name', 'description', 'n', 'm', 'products', 'start_date',
+              'end_date', 'is_active']
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(promo_type='n_for_m')
+
+
+@admin.register(FixedPricePromo)
+class FixedPricePromoAdmin(admin.ModelAdmin):
+    list_display = ['name', 'fixed_price', 'is_active',
+                    'start_date', 'end_date']
+    list_filter = ['is_active', 'start_date', 'end_date']
+    search_fields = ['name', 'description']
+    filter_horizontal = ['products']
+    fields = ['name', 'description', 'fixed_price', 'products', 'start_date',
+              'end_date', 'is_active']
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(promo_type='fixed_price')
