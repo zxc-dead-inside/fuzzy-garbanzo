1e5f5d487f0aa870f9f1c9b570c41b6338bf910b
zxc-dead-inside <maks.arhipkin@mail.ru>
2025-05-30 01:06:09 +1000

    elastic search engine

---
+++ market/catalog/documents.py
@@ -0,0 +1,24 @@
+       from django_elasticsearch_dsl import Document, fields, Index
+from django_elasticsearch_dsl.registries import registry
+from market.catalog.models import Product
+
+product_index = Index('products')
+product_index.settings(
+    number_of_shards=1,
+    number_of_replicas=0
+)
+
+
+@registry.register_document
+class ProductDocument(Document):
+    category_name = fields.TextField(attr='category.name')
+
+    class Index:
+        name = 'products'
+
+    class Django:
+        model = Product
+        fields = [
+            'name',
+            'description',
+        ]
